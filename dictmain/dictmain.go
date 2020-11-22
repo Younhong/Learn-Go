55fab38bcdbfbc88f207c18b85b9df9114123038
younhong <younhong@kakao.com>
2020-11-22 19:27:16 +0900

    url check

---
+++ dictmain/dictmain.go
@@ -0,0 +1,41 @@
+package dictmain
+
+import (
+	"fmt"
+
+	"github.com/younhong/Learn-Go/mydict"
+)
+
+func Dictmain() {
+	// dictionary := mydict.Dictionary{"first": "First Word"}
+	// definition, err := dictionary.Search("first")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
+	dictionary := mydict.Dictionary{}
+	// err := dictionary.Add("hello", "Greeting")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// definition, err := dictionary.Search("hello")
+	// fmt.Println(definition)
+	// err2 := dictionary.Add("hello", "Greeting")
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+	dictionary.Add("hello", "first")
+	// err := dictionary.Update("hello", "second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search("hello")
+	dictionary.Delete("hello")
+	word2, error := dictionary.Search("hello")
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(word2)
+	}
+}
--- main.go
+++ main.go
@@ -1,41 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/younhong/Learn-Go/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request Failed")
+
 func main() {
-	// dictionary := mydict.Dictionary{"first": "First Word"}
-	// definition, err := dictionary.Search("first")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println(definition)
-	// }
-	dictionary := mydict.Dictionary{}
-	// err := dictionary.Add("hello", "Greeting")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// definition, err := dictionary.Search("hello")
-	// fmt.Println(definition)
-	// err2 := dictionary.Add("hello", "Greeting")
-	// if err2 != nil {
-	// 	fmt.Println(err2)
-	// }
-	dictionary.Add("hello", "first")
-	// err := dictionary.Update("hello", "second")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// word, _ := dictionary.Search("hello")
-	dictionary.Delete("hello")
-	word2, error := dictionary.Search("hello")
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		fmt.Println(word2)
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode >= 400 {
+		return errRequestFailed
+	}
+
+	return nil
 }
