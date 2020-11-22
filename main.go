9cab108a4e731d80ed8036b49fdd3bc1dba61775
younhong <younhong@kakao.com>
2020-11-22 19:04:39 +0900

    update

--- main.go
+++ main.go
@@ -7,7 +7,35 @@ import (
 )
 
 func main() {
+	// dictionary := mydict.Dictionary{"first": "First Word"}
+	// definition, err := dictionary.Search("first")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
 	dictionary := mydict.Dictionary{}
-	dictionary["hello"] = "hello"
-	fmt.Println(dictionary["hello"])
+	// err := dictionary.Add("hello", "Greeting")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// definition, err := dictionary.Search("hello")
+	// fmt.Println(definition)
+	// err2 := dictionary.Add("hello", "Greeting")
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+	dictionary.Add("hello", "first")
+	// err := dictionary.Update("hello", "second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search("hello")
+	dictionary.Delete("hello")
+	word2, error := dictionary.Search("hello")
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(word2)
+	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -1,3 +1,51 @@
 package mydict
 
+import "errors"
+
 type Dictionary map[string]string
+
+var (
+	errNotFound   = errors.New("Not found")
+	errWordExists = errors.New("Word already exists")
+	errCantUpdate = errors.New("Can't update non-existing word")
+)
+
+// Search word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add Word
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+
+	return nil
+}
+
+// Update Word
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+
+	return nil
+}
+
+// Delete Word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
